b9b6f6c7e8480f4ab5f1d8fe1bdb3faf6f3c1551
borkild <ben.orkild@utah.edu>
2025-12-18 15:58:44 -0700

    created script to start making KiNet architecture

---
+++ dynamic_network_architectures/architectures/kinet.py
@@ -0,0 +1,16 @@
+import torch
+
+from dynamic_network_architectures.architectures.abstract_arch import (
+    AbstractDynamicNetworkArchitectures,
+    test_submodules_loadable,
+)
+
+from dynamic_network_architectures.initialization.weight_init import InitWeights_He
+
+
+
+class PlainConvKiNet(AbstractDynamicNetworkArchitectures):
+    pass
+
+
+
